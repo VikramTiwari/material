97a857182a1a97c971fdc7e0fd20c28ce5f9304a
Vikram <vikramtheone1@gmail.com>
2015-07-30 18:16:49 -0700

    material design on home page

--- public/controllers/material.js
+++ public/controllers/material.js
@@ -1,10 +1,10 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.material', ['mean.system'])
-.config(['$viewPathProvider', function($viewPathProvider) {
-  $viewPathProvider.override('system/views/index.html', 'material/views/index.html');
-}]);
+// angular.module('mean.material', ['mean.system'])
+// .config(['$viewPathProvider', function($viewPathProvider) {
+//   $viewPathProvider.override('system/views/index.html', 'material/views/index.html');
+// }]);
 
 angular.module('mean.material').controller('MaterialController', ['$scope', 'Global', 'Material',
 	function($scope, Global, Material) {
