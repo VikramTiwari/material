70ea0325706ac5e0843e34cdde91873d46e516eb
Vikram Tiwari <vikramtheone1@gmail.com>
2015-09-08 11:57:53 -0700

    all cleaned up
    reduced overall size for the package

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ var Module = require('meanio').Module;
 
 var Material = new Module('material');
 
-/*
- * All MEAN packages require registration
- * Dependency injection is used to define required modules
- */
-
 // added system package in dependency to sync package loading
 Material.register(function(app, auth, database, circles, system) {
 
@@ -36,39 +31,7 @@ Material.register(function(app, auth, database, circles, system) {
 		absolute: false
 	});
 
-	//We are adding a link to the main menu for all authenticated users
-	Material.menus.add({
-		title: 'Material',
-		link: 'material sample page',
-		roles: ['authenticated'],
-		menu: 'main'
-	});
-
-	// agreegated material.css which can be modified to place your own theme configs
-	Material.aggregateAsset('css', 'material.css');
 	Material.angularDependencies(['ngMaterial']);
-
-	/**
-	  //Uncomment to use. Requires meanio@0.3.7 or above
-	  // Save settings with callback
-	  // Use this for saving data from administration pages
-	  Material.settings({
-	      'someSetting': 'some value'
-	  }, function(err, settings) {
-	      //you now have the settings object
-	  });
-
-	  // Another save settings example this time with no callback
-	  // This writes over the last settings.
-	  Material.settings({
-	      'anotherSettings': 'some value'
-	  });
-
-	  // Get settings. Retrieves latest saved settigns
-	  Material.settings(function(err, settings) {
-	      //you now have the settings object
-	  });
-	  */
 
 	return Material;
 });
--- public/services/material.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('mean.material').factory('Material', [
-  function() {
-    return {
-      name: 'material'
-    };
-  }
-]);
